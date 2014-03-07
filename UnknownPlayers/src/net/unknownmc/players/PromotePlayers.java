80a6f8eae043076fec8b6d7e1d3374c29967d54b
DeprecatedNether <deprecatednether@gmail.com>
2014-03-07 19:13:20 +0100

    Use Playtime instead of FileConfiguration - see 4d1f0c1e35

--- UnknownPlayers/src/net/unknownmc/players/PromotePlayers.java
+++ UnknownPlayers/src/net/unknownmc/players/PromotePlayers.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 
 import org.bukkit.Bukkit;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -21,45 +20,38 @@ public class PromotePlayers extends BukkitRunnable {
     public void run() {
     	for (Player pl : Bukkit.getOnlinePlayers()) {
     		if (!pl.hasPermission("unknownmc.player")) {
-    			File folder = UnknownPlayers.folder;
-    			String name = pl.getName().toLowerCase() + ".yml";
-    			File confF = new File(folder, name);
-    			if (!confF.exists()) {
-    				UnknownPlayers.log.severe(confF.getAbsolutePath() + " (" + pl.getName() + ") doesn't exist!");
-    			} else {
-    				FileConfiguration stats = YamlConfiguration.loadConfiguration(confF);
-    				long playtime = stats.getLong("playtime");
-    				long current = System.currentTimeMillis() - stats.getLong("lastjoin");
-    				playtime = playtime + current;
-    				long reqtime = UnknownPlayers.config.getLong("rankup.time") * 60 * 1000; //Time is in minutes. Turn it into seconds, then millis
-    				if (playtime >= reqtime) {
-    					String group = UnknownPlayers.config.getString("rankup.group");
-    					Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "mansave");
-    					String path = "users." + pl + ".group";
-    					File gmDir = new File(Bukkit.getServer().getPluginManager().getPlugin("GroupManager").getDataFolder(), File.separator + "worlds" + File.separator + UnknownPlayers.config.getString("default-world"));
-    					File users = new File(gmDir, "users.yml");
-    					YamlConfiguration usrs;
-    					if (!users.isFile()) {
-    						UnknownPlayers.log.severe(users.getAbsolutePath() + " doesn't exist!");
-    						break;
-    					} else {
-    						usrs = YamlConfiguration.loadConfiguration(users);
-    					}
-    					if (!usrs.isString(path)) {
-    						break;
-    					}
-    					
-    					usrs.set(path, group);
-    					try {
-    						usrs.save(users);
-    					} catch (IOException e) {
-    						e.printStackTrace();
-    					}
-    					
-    					Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "manload");
-    					UnknownPlayers.log.info("Promoting " + pl.getName() + " to Player.");
-    				}
-    			}
+    			Playtime play = new Playtime(pl.getName());
+				long playtime = play.getPlayTime();
+				long current = System.currentTimeMillis() - play.getLastJoinTime();
+				playtime = playtime + current;
+				long reqtime = UnknownPlayers.config.getLong("rankup.time") * 60 * 1000; //Time is in minutes. Turn it into seconds, then millis
+				if (playtime >= reqtime) {
+					String group = UnknownPlayers.config.getString("rankup.group");
+					Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "mansave");
+					String path = "users." + pl + ".group";
+					File gmDir = new File(Bukkit.getServer().getPluginManager().getPlugin("GroupManager").getDataFolder(), File.separator + "worlds" + File.separator + UnknownPlayers.config.getString("default-world"));
+					File users = new File(gmDir, "users.yml");
+					YamlConfiguration usrs;
+					if (!users.isFile()) {
+						UnknownPlayers.log.severe(users.getAbsolutePath() + " doesn't exist!");
+						break;
+					} else {
+						usrs = YamlConfiguration.loadConfiguration(users);
+					}
+					if (!usrs.isString(path)) {
+						break;
+					}
+					
+					usrs.set(path, group);
+					try {
+						usrs.save(users);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					
+					Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "manload");
+					UnknownPlayers.log.info("Promoting " + pl.getName() + " to Player.");
+				}
     		}
     	}
     }
