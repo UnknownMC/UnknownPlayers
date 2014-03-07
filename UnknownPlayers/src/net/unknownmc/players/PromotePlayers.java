f167fbf315a43be84b90ac2ec559d962f440bc5d
DeprecatedNether <deprecatednether@gmail.com>
2014-03-07 18:05:06 +0100

    Use GroupManager files instead of commands for promoting

--- UnknownPlayers/src/net/unknownmc/players/PromotePlayers.java
+++ UnknownPlayers/src/net/unknownmc/players/PromotePlayers.java
@@ -1,6 +1,7 @@
 package net.unknownmc.players;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -32,8 +33,30 @@ public class PromotePlayers extends BukkitRunnable {
     				playtime = playtime + current;
     				long reqtime = UnknownPlayers.config.getLong("rankup.time") * 60 * 1000; //Time is in minutes. Turn it into seconds, then millis
     				if (playtime >= reqtime) {
-    					String cmd = String.format(UnknownPlayers.config.getString("rankup.command"), pl.getName());
-    					Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), cmd);
+    					String group = UnknownPlayers.config.getString("rankup.group");
+    					Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "mansave");
+    					String path = "users." + pl + ".group";
+    					File gmDir = new File(Bukkit.getServer().getPluginManager().getPlugin("GroupManager").getDataFolder(), File.separator + "worlds" + File.separator + "unknownmc");
+    					File users = new File(gmDir, "users.yml");
+    					YamlConfiguration usrs;
+    					if (!users.isFile()) {
+    						UnknownPlayers.log.severe(users.getAbsolutePath() + " doesn't exist!");
+    						break;
+    					} else {
+    						usrs = YamlConfiguration.loadConfiguration(users);
+    					}
+    					if (!usrs.isString(path)) {
+    						break;
+    					}
+    					
+    					usrs.set(path, group);
+    					try {
+    						usrs.save(users);
+    					} catch (IOException e) {
+    						e.printStackTrace();
+    					}
+    					
+    					Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "manload");
     					UnknownPlayers.log.info("Promoting " + pl.getName() + " to Player.");
     				}
     			}
