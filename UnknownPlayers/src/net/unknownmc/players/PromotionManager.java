476e9c6bc12949c7481d0dff683300cb38f0cb5d
DeprecatedNether <deprecatednether@gmail.com>
2014-04-03 17:44:39 +0200

    Use PromotionManager... forgot to commit about a month ago lol

---
+++ UnknownPlayers/src/net/unknownmc/players/PromotionManager.java
@@ -0,0 +1,17 @@
+package net.unknownmc.players;
+
+public class PromotionManager {
+	
+	String pl;
+	boolean commands;
+	
+	public PromotionManager(String player) {
+		pl = player;
+	}
+	
+	public void setNewGroup(String group) {
+		if (commands) {
+			
+		}
+	}
+}
